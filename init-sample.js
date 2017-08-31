133d365326a306a9b452e7a32ca29dcc325b31ec
Ken Yasue <ken.yasue@clover-studio.com>
2017-08-31 14:05:13 +0200

    Update init-sample, and add some debug outputs

--- init-sample.js
+++ init-sample.js
@@ -1,11 +1,97 @@
 const Const = require('./constants');
 
 module["exports"] = {
-
+    port: 8888,
+    baseURL:"",
+    endpointURL:"/allbot",
+    webhookReceiverURL: "/webhook",
+    fileSenderURL: "/file",
+    downloadPath: "./downloads",
+    loglevel:"debug",
     service : [
         {
-            provider:Const.facebook
-        }
+            identifier:'facebook',
+            provider:Const.facebook,
+            config:{
+                verifyToken: '',
+                accessToken: '',
+                pageId:'',
+                replyToBot: false 
+           }
+        },
+        {
+            identifier:'line',
+            provider:Const.line,
+            config:{
+                channelSecret: '',
+                channelAccessToken: '',
+                replyToBot: false // keep this false if you don't know 
+            }
+        },
+        {
+            identifier:'slack',
+            provider:Const.slack,
+            config:{
+                incomingWebHookURL: '',
+                appId: '',
+                botToken: '',
+                replyToBot: false // keep this false if you don't know 
+            }
+        },
+        {
+            identifier:'skype',
+            provider:Const.skype,
+            config:{
+                appId: '',
+                appPassword: '',
+            }
+        },
+        {
+            identifier:'telegram',
+            provider:Const.telegram,
+            config:{
+                botToken:''
+            }
+        },
+        {
+            identifier:'kik',
+            provider:Const.kik,
+            config:{
+                userName: '',
+                apiKey: ''
+            }
+        },
+        {
+            identifier:'viber',
+            provider:Const.viber,
+            config:{
+                apiKey: '',
+                botName: '',
+                botAvatar: ''
+            }
+        },
+        {
+            identifier:'sms',
+            isTest: true,
+            provider:Const.twilio,
+            config:{
+                number: '',
+                accountSid: '',
+                authToken: ''
+            }
+        },
+        {
+            identifier:'twitter',
+            provider:Const.twitter,
+            config:{
+                consumerSecret: '',
+                consumerKey: '',
+                token: '',
+                tokenSecret: '',
+            }
+        },
+
     ]
 
 };
+
